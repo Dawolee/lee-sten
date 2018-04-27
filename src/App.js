feb4f255e14d8cf5d2442ec287d25907f0323491
Da Woon Lee <daniel.lee9415@gmail.com>
2018-04-26 23:40:42 -0400

    added audio recording feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import KeyBoard from './keyBoard';
+import RecorderButtons from './recorderButtons';
+import Canvas from './canvas';
+import audioHelperFunc from './audioAPIHelper';
 
 class App extends Component {
+  componentDidMount() {
+    audioHelperFunc();
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Lee-sten: Create some sick beats yo!</h1>
+          <h1 className="App-title">Lee-sten : Da Woon and Only</h1>
+          <Canvas />
         </header>
         <KeyBoard />
+        <RecorderButtons />
       </div>
     );
   }
